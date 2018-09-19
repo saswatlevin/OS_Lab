7ef6879a37680ad449ad2b2fa19b905c3e0aa1e0
Saswat Levin <saswatlevin@gmail.com>
2018-09-19 14:47:41 +0530

    corrected program 3

--- Week5/w5_3.c
+++ Week5/w5_3.c
@@ -1,41 +1,6 @@
 #include <stdio.h>
 #include <pthread.h>
 int n=0;
-/*
-
-    #include<stdio.h>
-     
-    main()
-    {
-       int n, i = 3, count, c;
-     
-       printf("Enter the number of prime numbers required\n");
-       scanf("%d",&n);
-     
-       if ( n >= 1 )
-       {
-          printf("First %d prime numbers are :\n",n);
-          printf("2\n");
-       }
-     
-       for ( count = 2 ; count <= n ;  )
-       {
-          for ( c = 2 ; c <= i - 1 ; c++ )
-          {
-             if ( i%c == 0 )
-                break;
-          }
-          if ( c == i )
-          {
-             printf("%d\n",i);
-             count++;
-          }
-          i++;
-       }         
-     
-       return 0;
-     
-*/
 
 void *prime_generator(void *param)
 {
@@ -66,7 +31,7 @@ void *prime_generator(void *param)
 
 int main()
 {
-//prime_generator();
+
 int ret_val=0;
 pthread_t thread;
 
